24b7e51c4faa0bac86ee8cba31d88f3a70ef1eae
Digital Mind <jremesz@gmail.com>
2022-03-20 23:39:23 +0100

    Footer date added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,17 @@
 import React from "react";
 
 const Footer = () => {
-  const currentYear = new Date(2022, 1, 20);
-  const date = currentYear.getFullYear();
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const day = date.toLocaleString("en-US", { day: "numeric" });
   const company = "Digital Mind Solutions";
   return (
     <div className="bg-slate-800 text-slate-300 text-center p-4 fixed bottom-0 left-0 right-0">
       <div>
         <p>
-          &copy; {date} All rights reserved by {company}.
+          &copy; {year} All rights reserved by {company}. Latest update on
+          {" " + day + " " + month + ", " + year}
         </p>
       </div>
     </div>
